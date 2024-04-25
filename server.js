95cece391ee15587387cc210209a7562161487cf
Daniel Arellano <danyzapopan12@gmail.com>
2024-04-25 11:25:21 -0600

    changes to render

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const userRoute = require('./routes/userRoutes.js');
 const recipeRoute = require('./routes/recipeRoutes.js');
 const categoryRoute = require('./routes/categoryRoutes.js');
